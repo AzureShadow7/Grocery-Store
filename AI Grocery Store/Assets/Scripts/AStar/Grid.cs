53acb510d75396fbe94528e9968004a9d1be1054
AzureShadow7 <106389001+AzureShadow7@users.noreply.github.com>
2024-04-22 14:48:41 +0100

    Walkable and unwalkable paths

---
+++ AI Grocery Store/Assets/Scripts/AStar/Grid.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    Node[,] grid;
+
+    public float nodeRadius;
+    public Vector2 gridWorldSize;
+    public LayerMask unwalkableMask;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint);
+            }
+        }
+    }
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
+    {
+        float percentageX = (worldPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
+        float percentageY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+
+        percentageX = Mathf.Clamp01(percentageX);
+        percentageY = Mathf.Clamp01(percentageY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentageX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentageY);
+        return grid[x, y];
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+        if (grid != null)
+        {
+            foreach (Node node in grid)
+            {
+                Gizmos.color = (node.walkable) ? Color.white : Color.cyan;
+                Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
+    }
+}
